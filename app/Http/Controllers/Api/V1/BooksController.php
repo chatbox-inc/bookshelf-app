07905c94659e536f98ff4d42b46018e3ff22ecea
Kazuki Tobita <kazukiti201@gmail.com>
2019-03-05 17:16:01 +0900

    Add validation

--- app/Http/Controllers/Api/V1/BooksController.php
+++ app/Http/Controllers/Api/V1/BooksController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Book;
+use Validator;
 
 class BooksController extends Controller
 {
@@ -16,6 +17,12 @@ class BooksController extends Controller
     public function index()
     {
         $request = request();
+
+        $validator = Validator::make($request->all(), [
+            'count' => [ 'sometimes', 'bail', 'integer', 'min:1', ],
+        ]);
+        if ($validator->fails()) return response([], 400);
+
         $books = Book::select('id', 'title', 'is_rental')->take($request->count)->get();
         return response($books, 200);
     }
@@ -28,6 +35,12 @@ class BooksController extends Controller
     public function detail()
     {
         $request = request();
+
+        $validator = Validator::make($request->all(), [
+            'id' => [ 'bail', 'required', 'integer', 'min:1', ],
+        ]);
+        if ($validator->fails()) return response([], 400);
+
         $book = Book::where('id', $request->id)->first();
         return response($book, 200);
     }
@@ -40,6 +53,19 @@ class BooksController extends Controller
     public function add()
     {
         $request = request();
+
+        $validator = Validator::make($request->all(), [
+            'title'         => [ 'required', 'string', ],
+            'url'           => [ 'required', 'url', ],
+            'img'           => [ 'required', 'url', ],
+            'description'   => [ 'required', 'string', ],
+            'isbn'          => [ 'required', 'string', 'regex:/^([0-9]{9}[0-9X]{1}|[0-9]{13})$/' ],
+            'author'        => [ 'required', 'string', ],
+            'publisher'     => [ 'required', 'string', ],
+            'published_at'  => [ 'required', 'date', ],
+            'is_rental'     => [ 'required', 'boolean', ],
+        ]);
+        if ($validator->fails()) return response([], 400);
 
         $book = Book::create([
             'title'         => $request->title,
