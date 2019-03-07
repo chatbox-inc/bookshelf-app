3cf115346ef25bace022b337c3494f8d66a3fa8f
Ayumi Matsubara <ayumimatsubara410@gmail.com>
2019-03-07 16:19:13 +0900

    ref #7 書籍一覧画面のAPIつなぎこみ

--- nuxtjs/store/index.js
+++ nuxtjs/store/index.js
@@ -1,4 +1,3 @@
-import axios from "axios"
 export const state = () => ({
     books: [],
 })
@@ -10,82 +9,9 @@ export const mutations = {
 }
 
 export const actions = {
-    // async load(ctx) {
-    //     const bookDetails = [
-    load(ctx){
-        axios.get("http://jsonplaceholder.typicode.com/posts").then((result)=>{
-            ctx.commit("set",result.data)
-        })
+    async load(ctx) {
+        const bookList = await this.$axios.$get(`${process.env.NUXT_APIDOMAIN}/api/v1/books`)
+        console.log(bookList)
+        ctx.commit("setBooks",bookList)
     }
-            // {
-            //     "id": 1,
-            //     "title": "PHPの本",
-            //     "detail": "PHPの本詳細",
-            //     "url": "内容",
-            //     "image": "内容",
-            //     "ISBN": "内容",
-            //     "author": "内容",
-            //     "publisher": "内容",
-            //     "publishedYear": "内容",
-            //     "rentalHistory": [
-            //         {
-            //             "rentalHistoryId": 1,
-            //             "rentalPersonName": "barbara",
-            //             "rentalDateFrom": "2019-03-01",
-            //             "rentalDateTo": "2019-03-10",
-            //         },
-            //         {
-            //             "rentalHistoryId": 2,
-            //             "rentalPersonName": "barbara2",
-            //             "rentalDateFrom": "2019-03-01",
-            //             "rentalDateTo": "2019-03-10",
-            //         },
-            //         {
-            //             "rentalHistoryId": 3,
-            //             "rentalPersonName": "barbara3",
-            //             "rentalDateFrom": "2019-03-01",
-            //             "rentalDateTo": "2019-03-10",
-            //         }
-            //     ]
-            // },
-            // {
-            //     "id": 2,
-            //     "title": "HTMLの本",
-            //     "detail": "HTMLの本詳細",
-            //     "url": "内容",
-            //     "image": "内容",
-            //     "ISBN": "内容",
-            //     "author": "内容",
-            //     "publisher": "内容",
-            //     "publishedYear": "内容",
-            //     "rentalHistory": [
-            //         {
-            //             "rentalHistoryId": 1,
-            //             "rentalPersonName": "barbara",
-            //             "rentalDateFrom": "2019-03-01",
-            //             "rentalDateTo": "2019-03-10",
-            //         }
-            //     ]
-            // },
-            // {
-            //     "id": 3,
-            //     "title": "CSSの本",
-            //     "detail": "CSSの本詳細",
-            //     "url": "内容",
-            //     "image": "内容",
-            //     "ISBN": "内容",
-            //     "author": "内容",
-            //     "publisher": "内容",
-            //     "publishedYear": "内容",
-            //     "rentalHistory": [
-            //         {
-            //
-            //         }
-            //     ]
-            // }
-        //]
-        // const ip = await this.$axios.$get(`${process.env.NUXT_APIDOMAIN}/api/v1/books`)
-        // ctx.commit("setBooks", bookDetails)
-        //bookItemsにipを渡してあげる？
-    //}
 }
